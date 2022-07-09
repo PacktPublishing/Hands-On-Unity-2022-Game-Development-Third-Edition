06fc9ea514ace19056fe1181456ddae589810322
Nicolas Borromeo <nicolas.borromeo@unity3d.com>
2022-07-09 11:59:31 +0100

    Chapter 9

---
+++ Chapter 03 to Chapter 19 (SuperShooter)/Assets/Chapter 09 (CSharp)/Scripts/Sight.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public class Sight : MonoBehaviour
+{
+    public float distance;
+    public float angle;
+    public LayerMask objectsLayers;
+    public LayerMask obstacleLayers;
+
+    public GameObject detectedObject;
+
+    void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, distance, objectsLayers);
+
+        detectedObject = null;
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Collider collider = colliders[i];
+            Vector3 directionToCollider = Vector3.Normalize(collider.bounds.center - transform.position);
+            float angleToCollider = Vector3.Angle(transform.forward, directionToCollider);
+
+            if (angleToCollider < angle)
+            {
+                if (!Physics.Linecast(transform.position, collider.bounds.center, out RaycastHit hit, obstacleLayers))
+                {
+                    Debug.DrawLine(transform.position, collider.bounds.center, Color.green);
+                    detectedObject = collider.gameObject;
+                    break;
+                }
+                else
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.red);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, distance);
+
+        Vector3 rightDirection = Quaternion.Euler(0, angle, 0) * transform.forward;
+        Gizmos.DrawRay(transform.position, rightDirection * distance);
+        
+        Vector3 leftDirection = Quaternion.Euler(0, -angle, 0) * transform.forward;
+        Gizmos.DrawRay(transform.position, leftDirection * distance);
+    }
+}
